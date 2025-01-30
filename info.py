ad057aca8b629c3d89dee35129df0e7bb8fce86a
unknown <iisa@yuhmak.com.ar>
2025-01-30 16:54:19 -0300

    cambios 30-01-2025 v1

---
+++ info.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+def info():
+    st.title("Abyss Dirver v1.0")
+    st.write("Abyss Dirver v1.0 es una app que permite buscar productos en E-Commerces, descargar su información en formato Excel y mostrar las imágenes de los productos.")
