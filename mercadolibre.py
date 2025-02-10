8ab02ac9674d599f2dbfa3b162cb6fda48f31bb8
unknown <iisa@yuhmak.com.ar>
2025-02-10 16:50:57 -0300

    cambios 10022025 02

--- mercadolibre.py
+++ mercadolibre.py
@@ -68,6 +68,11 @@ def mercadolibre():
 def extraer_url(query, limit_pages, limit_products):
     options = Options()
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+
+    options.add_argument("--headless")  # Ejecutar en modo headless
+    options.add_argument("--no-sandbox")  # Necesario en entornos sin GUI
+    options.add_argument("--disable-dev-shm-usage")  # Evitar problemas de memoria
+    options.add_argument("--disable-gpu")  # Desactivar GPU para evitar errores
     
     driver = webdriver.Chrome(options=options)
 
@@ -131,6 +136,10 @@ def extraer_info(query, limit_pages, limit_products):
     links_productos = extraer_url(query, limit_pages, limit_products)
 
     options = Options()
+    options.add_argument("--headless")  # Ejecutar en modo headless
+    options.add_argument("--no-sandbox")  # Necesario en entornos sin GUI
+    options.add_argument("--disable-dev-shm-usage")  # Evitar problemas de memoria
+    options.add_argument("--disable-gpu")  # Desactivar GPU para evitar errores
     driver = webdriver.Chrome(options=options)
 
     nombre =  []
